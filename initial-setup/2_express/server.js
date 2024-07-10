bbeb712917639d310224d3b55c33fb122adb89d2
DanielMoraisDev <danielfelipe2905@gmail.com>
2024-07-10 08:28:17 -0300

    updated 2_express

--- initial-setup/2_express/server.js
+++ initial-setup/2_express/server.js
@@ -1,36 +1,55 @@
-const express = require('express')
-const PORT = 8080 || 3030
+const express = require("express");
+const PORT = 8080 || 3030;
 
-const app = express()
+const app = express();
+
+app.use(express.json());
 
 app.get("/users", (req, res) => {
-    res.json({ message: [
-        "João Gomes",
-        "José Camoes",
-        "Adelia De Jesus"
-    ]})
-})
+  try {
+    const { nome, idade } = req.query;
+
+    if (!nome || !idade) {
+      res.status(500).json({ message: "Ausente de dados" });
+    }
+
+    res.send({ message: {nome, idade} });
+  } catch (error) {
+    console.log("[USERS GET FAIL] Error: " + error);
+  }
+});
 
 app.post("/users", (req, res) => {
-    res.status(201).json({ message: "POST"})
-})
+  try {
+    const user = {
+      nome: req.body.nome,
+      idade: req.body.idade,
+    };
+
+    if (!user.nome || !user.idade) {
+      res.status(500).json({ message: "Ausente de dados" });
+    }
+
+    res.status(201).json({ message: user });
+  } catch (error) {
+    console.log("[USERS POST FAIL] Error: " + error);
+  }
+});
 
 app.delete("/users", (req, res) => {
-    res.status(204).json({ message: "DELETE"})
-})
+  res.status(204).json({ message: "DELETE" });
+});
 
 app.patch("/users", (req, res) => {
-    res.status(200).json({ message: "PATCH"})
-})
+  res.status(200).json({ message: "PATCH" });
+});
 
-app.put("/users", (req, res) => {
-    res.json({ message: [
-        "João Gomes",
-        "José Camoes",
-        "Adelia De Jesus"
-    ]})
-})
+app.put("/users/:id/:cpf", (req, res) => {
+  const { id, cpf } = req.params;
+
+  res.json({ message: ["João Gomes", "José Camoes", "Adelia De Jesus"] });
+});
 
 app.listen(PORT, () => {
-    console.log('Servidor rodando na porta: ' + PORT)
-})
+  console.log("Servidor rodando na porta: " + PORT);
+});
